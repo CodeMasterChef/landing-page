ceaacd4a2c180109f6d7cedab7de270f93d46031
Ninh Le <ninh.le@loyaworld.com>
2019-10-13 12:30:01 +0700

    add translation

--- assets/js/index.js
+++ assets/js/index.js
@@ -38,3 +38,40 @@ $('header ul li a').click(function (e) {
     closeMenu()
 
 })
+
+var translator;
+$(document).ready(function () {
+    var dict = {
+        Home: {
+            vi: 'Trang chủ'
+        },
+        'About Us' :{
+            vi: 'Về chúng tôi'
+        },
+        Products : {
+            vi: 'Sản phẩm'
+        },
+        Careers : {
+            vi: 'Nghề nghiệp'
+        },
+        Contact : {
+            vi: 'Liên hệ'
+        }
+
+
+    }
+    translator = $('body').translate({ lang: 'en', t: dict });
+
+    $('#englishIcon').hide();
+
+    $('#vietnameseIcon').click(()=>{
+        translator.lang('vi');
+        $('#englishIcon').show();
+        $('#vietnameseIcon').hide();
+    })
+    $('#englishIcon').click(()=>{
+        translator.lang('en');
+        $('#vietnameseIcon').show();
+        $('#englishIcon').hide();
+    })
+})
