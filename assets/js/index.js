18a1fa382bee8d82a93edec08939d7fde9df32ec
Ninh Le <ninh.le@loyaworld.com>
2019-10-18 21:50:13 +0700

    update expertise session

--- assets/js/index.js
+++ assets/js/index.js
@@ -42,19 +42,24 @@ $('header ul li a').click(function (e) {
 var translator;
 $(document).ready(function () {
     var dict = {
-        Home: {
+        home: {
+            en: 'Home',
             vi: 'Trang chủ'
         },
-        'About Us' :{
+        about_us :{
+            en: 'About Us',
             vi: 'Về chúng tôi'
         },
-        Products : {
+        products : {
+            en: 'Products',
             vi: 'Sản phẩm'
         },
-        Careers : {
+        careers : {
+            en :'Careers',
             vi: 'Nghề nghiệp'
         },
-        Contact : {
+        contact : {
+            en: 'Contact',
             vi: 'Liên hệ'
         },
         'our_expertise': {
@@ -63,11 +68,30 @@ $(document).ready(function () {
         },
         technology_solutions: {
             en: 'Technology Solutions',
-            vi: 'Giải pháp công nghệ',
+            vi: 'Giải pháp Công Nghệ',
         },
         technology_solutions_description: {
             en: 'With a strong technology knowledge, we build technology platforms for businesses and work together to build products with startups. This helps businesses and startups focus more on their business model.',
             vi: 'Với nên tảng công nghệ, chúng tôi hướng tới xây dựng nền tảng công nghệ cho các doanh nghiệp và đồng hành xây dựng sản phẩm cùng các Startup. Từ đó giúp cho các doanh nghiêp và các startup tập trung hơn vào mô hình kinh doanh của mình.',
+        },
+        business_solution: {
+            en: 'Business Solution',
+            vi: 'Giải pháp Kinh Doanh',
+        },
+        business_solution_description: {
+            en: 'We provide business solutions and operational resources. FIOP want to build a business workforce so that businesses and startups can focus on operating their business plan in the simplest way.',
+            vi: 'Chúng tôi cung cấp giải pháp kinh doanh và nguồn lực vận hành. FIOP hướng tới xây dựng đội ngũ nhân lực kinh doanh để các Doanh nghiệp và Startup có thể tập trung vận hành kế hoạch kinh doanh của mình một cách đơn giản nhất.'
+        },
+        investment_solutions: {
+            en: 'Investment Solutions',
+            vi: 'Giải pháp Đầu Tư',
+        },
+        investment_solutions_description: {
+            en: 'With an investment experience, we look forward to consulting and connecting to help businesses and startups. From there, approach to and promote investment more effectively.',
+            vi: 'Với kinh nghiệp đầu tư, chúng tôi mong muốn tư vấn và kết nối nhằm trợ giúp các doanh nghiệp và Startup. Từ đó tiến tới tiếp cận và xúc tiến đầu tư hiệu quả hơn.'
+
+
+
         }
 
 
